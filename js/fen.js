41cf1b7e5b4abaa07c5d9ad7521c3563b250ba94
doctone <samjojames@gmail.com>
2021-12-12 21:45:04 +0000

    updated functioned to handle state

--- js/fen.js
+++ js/fen.js
@@ -3,28 +3,30 @@
 
 export default class Position {
     constructor() {
-        this.startPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
-        this.clickedSquare = undefined;
-        this.fenBox = document.getElementById('FEN');
-        this.fenButton = document.getElementById('fen-button')
-        this.cells = document.querySelectorAll('.cell')
-        this.pieces = {
-            P: "&#9817;",
-            p: "&#9823;",
-            k: '&#9818;',
-            K: '&#9812;',
-            q: '&#9819;',
-            Q: '&#9813;',
-            b: '&#9821;',
-            B: '&#9815;',
-            r: '&#9820;',
-            R: '&#9814;',
-            n: '&#9822;',
-            N: '&#9816;'
+        this.state = {
+            startPosition: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+            clickedSquare: undefined,
+            fenBox: document.getElementById('FEN'),
+            fenButton: document.getElementById('fen-button'),
+            cells: document.querySelectorAll('.cell'),
+            pieces: {
+                P: "&#9817;",
+                p: "&#9823;",
+                k: '&#9818;',
+                K: '&#9812;',
+                q: '&#9819;',
+                Q: '&#9813;',
+                b: '&#9821;',
+                B: '&#9815;',
+                r: '&#9820;',
+                R: '&#9814;',
+                n: '&#9822;',
+                N: '&#9816;'
+            },
         }
-        this.renderPieces(this.startPosition);
-        this.cells.forEach(cell => {cell.addEventListener('click', this.movePiece)});
-        this.fenButton.addEventListener('click', this.fillBoard)
+        this.renderPieces(this.state.startPosition);
+        this.state.cells.forEach(cell => {cell.addEventListener('click', this.movePiece)});
+        this.state.fenButton.addEventListener('click', this.fillBoard)
 
     }
     // Make 2d Array
@@ -44,7 +46,7 @@ export default class Position {
 
     renderPieces(FEN) {
         // update current fen input box
-        this.fenBox.value = FEN;
+        this.state.fenBox.value = FEN;
         // create full array
         FEN = this.fillFenArray(FEN);
 
@@ -52,10 +54,10 @@ export default class Position {
         for (let i = 0; i < FEN.length; i++) {
             for (let s = 0; s < 8; s++) {
                 const pieceImg = FEN[i].split('')[s]
-                if (this.pieces[pieceImg]) {
+                if (this.state.pieces[pieceImg]) {
                     const square = document.getElementById(`${i}${s}`)
                     const piece = document.createElement('h1')
-                    piece.innerHTML = this.pieces[pieceImg]
+                    piece.innerHTML = this.state.pieces[pieceImg]
                     piece.setAttribute('class', 'piece')
                     square.appendChild(piece)
                 }
@@ -79,30 +81,32 @@ export default class Position {
     }
 
     // moving piece from square to square
-    movePiece() {
-        const cell = this;
-        // if you click on the same square twice, do nothing
-        if (cell == this.clickedSquare) return null;
+    movePiece(event) {
+        const cell = event.target;
+        console.log(cell);
+    //     // if you click on the same square twice, do nothing
+    //     if (cell == this.state.clickedSquare) return null;
 
-        if (this.clickedSquare) {
-            // put piece down
-            console.log('putting piece down ' + this.clickedSquare.firstChild);
-            this.clickedSquare.classList.remove('clicked');
-            if (cell.firstChild) {
-                cell.removeChild(cell.firstChild);
-                cell.appendChild(this.clickedSquare.firstChild);
-            }
-            else {
-                cell.appendChild(this.clickedSquare.firstChild);
-            }
-            this.clickedSquare = undefined;
-        }
-        else {
-            if (cell.firstChild) {
-                this.clickedSquare = cell;
-                console.log('picking piece up ' + this.clickedSquare.firstChild);
-                this.clickedSquare.classList.add('clicked');
-            }
-        }
+    //     if (this.clickedSquare) {
+    //         // put piece down
+    //         console.log('putting piece down ' + this.clickedSquare.firstChild);
+    //         this.clickedSquare.classList.remove('clicked');
+    //         if (cell.firstChild) {
+    //             cell.removeChild(cell.firstChild);
+    //             cell.appendChild(this.clickedSquare.firstChild);
+    //         }
+    //         else {
+    //             cell.appendChild(this.clickedSquare.firstChild);
+    //         }
+    //         this.clickedSquare = undefined;
+    //     }
+    //     else {
+    //         if (cell.firstChild) {
+    //             this.clickedSquare = cell;
+    //             console.log('picking piece up ' + this.clickedSquare.firstChild);
+    //             this.clickedSquare.classList.add('clicked');
+    //         }
+    //     }
+    // }
     }
 }
